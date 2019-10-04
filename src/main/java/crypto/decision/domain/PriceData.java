0bde53164494da916df5caabfa93aadab18f6f08
Anil Kadimisetty <anil@chainalysis.com>
2019-10-04 09:52:51 -0700

    Added lombok integration

--- src/main/java/crypto/decision/domain/PriceData.java
+++ src/main/java/crypto/decision/domain/PriceData.java
@@ -1,31 +1,13 @@
 package crypto.decision.domain;
 
-public class PriceData {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
+@AllArgsConstructor
+@Getter
+@Slf4j
+public class PriceData {
     private Double btcPrice;
     private Double ethPrice;
-
-    public PriceData() { }
-
-    public PriceData(Double btcPrice, Double ethPrice) {
-        this.btcPrice = btcPrice;
-        this.ethPrice = ethPrice;
-    }
-
-    public Double getBtcPrice() {
-        return btcPrice;
-    }
-
-    public void setBtcPrice(Double btcPrice) {
-        this.btcPrice = btcPrice;
-    }
-
-    public Double getEthPrice() {
-        return ethPrice;
-    }
-
-    public void setEthPrice(Double ethPrice) {
-        this.ethPrice = ethPrice;
-    }
-
 }
